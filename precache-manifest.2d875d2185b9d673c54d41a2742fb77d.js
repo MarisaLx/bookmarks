cada89ed78d90ec493fbfcd02d15207350163633
Marisa <marisa.bernardo@talkdesk.com>
2020-10-22 11:57:30 +0100

    Updates

---
+++ precache-manifest.2d875d2185b9d673c54d41a2742fb77d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05772c2e069377df3c5153baa6d3787",
+    "url": "/bookmarks/index.html"
+  },
+  {
+    "revision": "0ef17da0c3a4b7f64cdc",
+    "url": "/bookmarks/static/css/main.522e30d3.chunk.css"
+  },
+  {
+    "revision": "a048f7b388fae69f113f",
+    "url": "/bookmarks/static/js/2.61012ef4.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/bookmarks/static/js/2.61012ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef17da0c3a4b7f64cdc",
+    "url": "/bookmarks/static/js/main.f460d742.chunk.js"
+  },
+  {
+    "revision": "932eeb574686490ffcae",
+    "url": "/bookmarks/static/js/runtime-main.5d27598f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bookmarks/static/media/logo.5d5d9eef.svg"
+  }
+]);
